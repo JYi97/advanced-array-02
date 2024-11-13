121d4b41f3cfe8da54cb0ca5efdda6667eaf7eda
Justin <yi.justin29@gmail.com>
2024-11-12 16:08:20 -0800

    Product Reduce Completed

--- problems/08-product-reduce.js
+++ problems/08-product-reduce.js
@@ -17,6 +17,9 @@ console.log(productWithReduce([4, 3])); // 12
 
 let productWithReduce = function(nums) {
     // Your code here
+    const product = nums.reduce((currentValue, nextVal) => currentValue *= nextVal);
+    return product;
+
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -25,4 +28,4 @@ try {
   module.exports = productWithReduce;
 } catch (e) {
   module.exports = null;
-}
+}
